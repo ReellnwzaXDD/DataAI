bdda6f68c8651794d710438290bb0a912c9a91ac
ReellnwzaXDD <patanin511@gmail.com>
2025-10-06 11:04:01 +0700

    Edit name

--- chatbot-ui/app/[locale]/layout.tsx
+++ chatbot-ui/app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "DataAI"
+const APP_DEFAULT_TITLE = "DataAI"
+const APP_TITLE_TEMPLATE = "%s - DataAI"
+const APP_DESCRIPTION = "DataAI NaJa"
 
 interface RootLayoutProps {
   children: ReactNode
